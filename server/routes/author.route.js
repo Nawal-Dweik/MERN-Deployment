7612a7d0d7c2f8cfbf889ba81059243c5b28e5c3
Nawal-Dweik <nawal_farouk@outlook.com>
2021-02-16 12:02:05 +0200

    initial commit

---
+++ server/routes/author.route.js
@@ -0,0 +1,6 @@
+const AuthorController = require('../controllers/author.controller');
+
+module.exports = (app) => {
+    app.post('/api/createNewAuthor', AuthorController.createAuthor);
+    app.get('/api/getAllAuthors', AuthorController.findAllAuthors);
+}
